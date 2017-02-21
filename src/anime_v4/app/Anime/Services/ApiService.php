dab9c05a35a5387b450461c7756c0e7cde26c635
Allen Chiu <allenchiu1322@users.noreply.github.com>
2017-02-21 17:32:33 +0800

    code cleanup

--- src/anime_v4/app/Anime/Services/ApiService.php
+++ src/anime_v4/app/Anime/Services/ApiService.php
@@ -1,28 +1,9 @@
 <?php
 namespace App\Anime\Services;
-//use App\Anime\Repositories\TitleRepository;
 
 use Illuminate\Http\Response;
 
 class ApiService {
-
-//    private $title;
-
-    /*
-    public function __construct(Title $title) {
-        $this->title = $title;
-    }
-     */
-
-//    public function load_test_data() {
-//        $title_repo = new TitleRepository();
-//        $data = $title_repo->get_all();
-//        return $data;
-//        /*
-//        $test_data = $this->title->get();
-//        return $test_data;
-//         */
-//    }
 
     public function format_output($code, $message, $body = '') {
         $ret = [
--- src/anime_v4/app/Http/Controllers/ApiController.php
+++ src/anime_v4/app/Http/Controllers/ApiController.php
@@ -6,15 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-//use App\Http\Controllers\Controller;
-//use App\Anime\Repositories\TitleRepository;
 use App\Anime\Services\ApiService;
 use App\Anime\Services\DataProcessService;
-
-/*
-use Anime\Repositories\TitleRepository;
-use App\Title;
- */
 
 class ApiController extends Controller
 {
@@ -23,88 +16,11 @@ class ApiController extends Controller
     protected $dataProcessService;
 
     public function __construct() {
-//        $this->titleRepository = $titleRepository;
-//        $this->api_service = new apiService;
         $this->apiService = new ApiService;
         $this->dataProcessService = new DataProcessService;
     }
-    /*
-    protected $titleRepository;
 
-    public function __construct(TitleRepository $titleRepository) {
-        $this->titleRepository = $titleRepository;
-    }
-     */
-
-    /*
-    //todo: 寫三種table的crud
-    public function test2() {
-        $title = new Title;
-        $data = [
-            'test_data' => $title->all(),
-        ];
-
-        return var_dump($data);
-        //return json_encode($data);
-    }
-
-    public function test3() {
-        $title = new Title;
-
-        $title->title = 'test' . rand(0, 1000);
-        $title->title_jp = 'test_jp';
-        $title->comment = '';
-
-        $ret = $title->save();
-
-        var_dump($title->id);
-
-    }
-
-    public function test4() {
-        $title = Title::firstOrNew(['title' => 'test614']);
-        if ($title->exists) {
-
-        } else {
-            $title->title_jp = 'test_jp';
-            $title->comment = '';
-            $title->save();
-        }
-
-        var_dump($title->id);
-        echo(time());
-
-    }
-
-    public function test5() {
-        $title = Title::firstOrNew(['title' => 'test462']);
-        $title->title_jp = 'test_jp' . rand(0,9999);
-        $title->comment = '';
-        $title->save();
-
-        echo(time());
-
-    }
-
-    public function test6() {
-        $title = Title::where('title', '=', 'test461')->delete();
-        var_dump($title);
-
-    }
-     */
-
-    public function test6() {
-        echo('orz');
-    }
-
-    /*
-    public function test7() {
-        $ret = $this->titleRepository->load_test_data();
-        var_dump($ret);
-    }
-     */
-
-    public function test8() {
+    public function build_api_param() {
         /*
         $param = [
             'action' => 'set',
@@ -130,14 +46,6 @@ class ApiController extends Controller
             ]
         ];
         return json_encode($param);
-    }
-
-    public function test9(Request $request) {
-        var_dump(json_decode($request->param));
-    }
-
-    public function test10() {
-        return $this->apiService->format_output('100', 'OK');
     }
 
     public function data_process(Request $request) {
